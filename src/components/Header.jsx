48d13139ee5be4102dfd1172328c525397b275fe
Leonardo Beltran <lcbeltran@eis.com.co>
2019-10-03 16:48:52 -0500

    se adiciona variables a sass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ import logo from '../assets/static/logo-platzi-video.png';
 import userIcon from '../assets/static/user.png';
 
 const Header = () => (
-  <header className='header'>
-    <img className='header__img' src={logo} alt='Logo Platzi Video' />
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='User' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <a href='/'>Cuenta</a>
-        </li>
-        <li>
-          <a href='/'>Cerrar Sesión</a>
-        </li>
-      </ul>
-    </div>
-  </header>
+	<header className='header'>
+		<img className='header__img' src={logo} alt='Logo Platzi Video' />
+		<div className='header__menu'>
+			<div className='header__menu--profile'>
+				<img src={userIcon} alt='User' />
+				<p>Perfil</p>
+			</div>
+			<ul>
+				<li>
+					<a href='/'>Cuenta</a>
+				</li>
+				<li>
+					<a href='/'>Cerrar Sesión</a>
+				</li>
+			</ul>
+		</div>
+	</header>
 );
 
 export default Header;
