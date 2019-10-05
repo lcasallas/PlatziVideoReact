9b03e94cc9064fcf4b369be0d0b913b179343ecf
Leonardo Beltran <lcbeltran@eis.com.co>
2019-10-04 22:22:51 -0500

    se agrega hooks y api, finalizacion de curso

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/icon-play.png';
 import plusIcon from '../assets/static/icon-plus.png';
 
-const CarouselItem = () => (
-  <div className='carousel-item'>
-    <img
-      className='carousel-item__img'
-      src='https://images.pexels.com/photos/1107666/pexels-photo-1107666.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-      alt='film'
-    />
-    <div className='carousel-item__details'>
-      <div>
-        <img
-          className='carousel-item__details--icon'
-          src={playIcon}
-          alt='reproducir'
-        />
-        <img
-          className='carousel-item__details--icon'
-          src={plusIcon}
-          alt='agregar'
-        />
-      </div>
-      <p className='carousel-item__details--title'>Titulo</p>
-      <p className='carousel-item__details--subtitle'>2019 16+ 114minutos</p>
-    </div>
-  </div>
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
+	<div className='carousel-item'>
+		<img className='carousel-item__img' src={cover} alt={title} />
+		<div className='carousel-item__details'>
+			<div>
+				<img
+					className='carousel-item__details--icon'
+					src={playIcon}
+					alt='reproducir'
+				/>
+				<img
+					className='carousel-item__details--icon'
+					src={plusIcon}
+					alt='agregar'
+				/>
+			</div>
+			<p className='carousel-item__details--title'>{title}</p>
+			<p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration}`}</p>
+		</div>
+	</div>
 );
+
+CarouselItem.propTypes = {
+	cover: PropTypes.string,
+	title: PropTypes.string,
+	year: PropTypes.number,
+	contentRating: PropTypes.string,
+	duration: PropTypes.number,
+};
 
 export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const API = 'http://localhost:3000/initialState';
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+	const initialState = useInitialState(API);
 
-    <Categories title='Recientes'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+	return (
+		<div className='App'>
+			<Header />
+			<Search />
+			{initialState.mylist.length > 0 && (
+				<Categories title='Mi lista'>
+					<Carousel>
+						{initialState.mylist.map(item => (
+							<CarouselItem key={item.id} {...item} />
+						))}
+					</Carousel>
+				</Categories>
+			)}
+			<Categories title='Tendencias'>
+				<Carousel>
+					{initialState.trends.map(item => (
+						<CarouselItem key={item.id} {...item} />
+					))}
+				</Carousel>
+			</Categories>
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+			<Categories title='Recientes'>
+				<Carousel>
+					{initialState.originals.map(item => (
+						<CarouselItem key={item.id} {...item} />
+					))}
+				</Carousel>
+			</Categories>
 
-    <Footer />
-  </div>
-);
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
